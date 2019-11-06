a4e7a7ef4022ebe32e71b187a509d0c6375ba81a
Yuci Gou <yuci.gou@gmail.com>
2019-11-06 23:04:21 +0000

    Add the QueryBox component

---
+++ src/components/QueryBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" name="query" id="query" v-model="query" />
+    <button @click="emitQuery">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: ""
+    };
+  },
+  methods: {
+    emitQuery() {
+      this.$emit("search", this.query);
+    }
+  }
+};
+</script>
---
+++ src/components/__tests__/QueryBox.test.js
@@ -0,0 +1,24 @@
+import { shallowMount } from "@vue/test-utils";
+import QueryBox from "../QueryBox";
+
+describe("QueryBox", () => {
+  it("renders an input text field", () => {
+    const wrapper = shallowMount(QueryBox);
+    expect(wrapper.find("input").is("input")).toBe(true);
+  });
+
+  it("renders a Search button", () => {
+    const wrapper = shallowMount(QueryBox);
+    expect(wrapper.find("button").text()).toContain("Search");
+  });
+
+  it("emits search event with the input text as payload when clicked", () => {
+    const query = "cancer";
+    const wrapper = shallowMount(QueryBox);
+    const input = wrapper.find("input");
+    input.setValue(query);
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(wrapper.emitted().search[0]).toEqual([query]);
+  });
+});
