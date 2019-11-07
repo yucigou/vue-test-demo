7d18b4bf340f3f35e21e34f1dcbd9a6008c6adf1
Yuci Gou <yuci.gou@gmail.com>
2019-11-07 11:25:34 +0000

    TDD Vuex module article

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import article from "./modules/article";
+
+const modules = { article };
 
 Vue.use(Vuex);
 
@@ -7,5 +10,5 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules
 });
