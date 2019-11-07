4846ab60d3903ec6995d6a690298f4fbb78a4704
Yuci Gou <yuci.gou@gmail.com>
2019-11-07 12:38:19 +0000

    Add ArticleItem component

---
+++ src/components/ArticleItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <div>
+      {{ item.title }}
+    </div>
+    <div>
+      {{ item.authorString }}
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    item: Object
+  }
+};
+</script>
---
+++ src/components/__tests__/ArticleItem.test.js
@@ -0,0 +1,15 @@
+import { shallowMount } from "@vue/test-utils";
+import ArticleItem from "../ArticleItem";
+import * as data from "./ArticleItem.json";
+
+describe("ArticleItem", () => {
+  it("renders the title and authors", () => {
+    const wrapper = shallowMount(ArticleItem, {
+      propsData: {
+        item: data
+      }
+    });
+    expect(wrapper.text()).toContain(data.title);
+    expect(wrapper.text()).toContain("Zhou P");
+  });
+});
